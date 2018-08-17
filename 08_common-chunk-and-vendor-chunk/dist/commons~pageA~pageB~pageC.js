8f0bb40d201a0555a3cccd2a108caf02a3f5de7d
Aaayang <iamajser@163.com>
2018-08-17 23:24:47 +0800

    提取公共代码

---
+++ 08_common-chunk-and-vendor-chunk/dist/commons~pageA~pageB~pageC.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["commons~pageA~pageB~pageC"],{
+
+/***/ "./src/util2.js":
+/*!**********************!*\
+  !*** ./src/util2.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\n\n//# sourceURL=webpack:///./src/util2.js?");
+
+/***/ })
+
+}]);
---
+++ 08_common-chunk-and-vendor-chunk/dist/commons~pageB~pageC.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["commons~pageB~pageC"],{
+
+/***/ "./src/util3.js":
+/*!**********************!*\
+  !*** ./src/util3.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\n\n//# sourceURL=webpack:///./src/util3.js?");
+
+/***/ })
+
+}]);
